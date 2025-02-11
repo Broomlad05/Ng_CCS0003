6786083e7a1bd22845e796144545d54947aff4d3
Broomlad05 <ngjustinelawrence.05@gmail.com>
2025-02-11 23:55:09 +0800

    Add files via upload

---
+++ Ng_CCS0003/ng_3_3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int num1, num2;
+    int sum, difference, product, remainder, quotient;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+    difference = num1 - num2;
+    product = num1 * num2;
+    remainder = num1 % num2;
+    quotient = num1 / num2;
+
+    cout << "\nResults:" << endl;
+    cout << "Sum: " << sum << endl;
+    cout << "Difference: " << difference << endl;
+    cout << "Product: " << product << endl;
+    cout << "Remainder: " << remainder << endl;
+    cout << "Quotient: " << quotient << endl;
+
+    return 0;
+}
+
---
+++ Ng_CCS0003/ng_3_4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip> 50
+
+using namespace std;
+
+int main() {
+
+    double phpAmount, usd, euro, yuan, koruna, krone, sheqel, dinar;
+
+    cout << "Enter Philippine peso: ";
+    cin >> phpAmount;
+
+    
+    usd = phpAmount / 43.33089;
+    euro = usd * 0.734719;
+    yuan = usd * 6.346934;
+    koruna = usd * 18.77263;
+    krone = usd * 5.449007;
+    sheqel = usd * 3.726334;
+    dinar = usd * 0.274588;
+
+    cout << fixed << setprecision(6); 
+    cout << "\nThe amount's equivalent to:\n";
+    cout << "US Dollar is \t: \t" << usd << endl;
+    cout << "Euro\t\t: \t" << euro << endl;
+    cout << "Yuan\t\t: \t" << yuan << endl;
+    cout << "Koruna\t\t: \t" << koruna << endl;
+    cout << "Krone\t\t: \t" << krone << endl;
+    cout << "Sheqel\t\t: \t" << sheqel << endl;
+    cout << "Dinar\t\t: \t" << dinar << endl;
+
+    return 0;
+}
