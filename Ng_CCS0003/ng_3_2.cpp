39d83a72f05f1a342bc0567ded38f88a1678e807
Broomlad05 <ngjustinelawrence.05@gmail.com>
2025-02-11 23:20:13 +0800

    Add files via upload

---
+++ Ng_CCS0003/ng_3_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std;
+
+int main(void)
+{
+    int Number = 10;       
+    char Letter = 'a';     
+    bool Result = true;    
+    float Float = 3.14;    
+
+    cout << "Variable Name: Number, Value: " << Number << endl;     
+    cout << "Variable Name: Letter, Value: " << Letter << endl;     
+    cout << "Variable Name: Result, Value: " << (Result ? "true" : "false") << endl; 
+    cout << "Variable Name: Float, Value: " << Float << endl;        
+
+    return 0; 
+}
